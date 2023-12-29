714a1e897bddc6099450cbcbe76f419e534cbaf9
ChiragAgg5k <110609663+ChiragAgg5k@users.noreply.github.com>
2023-12-29 09:10:36 +0530

    almost complete

---
+++ src/app/new-text.tsx
@@ -0,0 +1,77 @@
+"use client";
+import React, {useEffect, useRef} from "react";
+
+export default function NewText({
+    containerRef,
+    text
+                                }:{
+    containerRef: React.RefObject<HTMLDivElement>;
+    text: string;
+}){
+
+    const boxRef = useRef<HTMLDivElement>(null);
+
+    const isClicked = useRef<boolean>(false);
+
+    const coords = useRef<{
+        startX: number,
+        startY: number,
+        lastX: number,
+        lastY: number
+    }>({
+        startX: 0,
+        startY: 0,
+        lastX: 0,
+        lastY: 0
+    })
+
+    useEffect(() => {
+        if (!boxRef.current || !containerRef.current) return;
+
+        const box = boxRef.current;
+        const container = containerRef.current;
+
+
+        const onMouseDown = (e: MouseEvent) => {
+            isClicked.current = true;
+            coords.current.startX = e.clientX;
+            coords.current.startY = e.clientY;
+        }
+
+        const onMouseUp = (e: MouseEvent) => {
+            isClicked.current = false;
+            coords.current.lastX = box.offsetLeft;
+            coords.current.lastY = box.offsetTop;
+        }
+
+        const onMouseMove = (e: MouseEvent) => {
+            if (!isClicked.current) return;
+
+            const nextX = e.clientX - coords.current.startX + coords.current.lastX;
+            const nextY = e.clientY - coords.current.startY + coords.current.lastY;
+
+            box.style.top = `${nextY}px`;
+            box.style.left = `${nextX}px`;
+        }
+
+        box.addEventListener('mousedown', onMouseDown);
+        box.addEventListener('mouseup', onMouseUp);
+        container.addEventListener('mousemove', onMouseMove);
+        container.addEventListener('mouseleave', onMouseUp);
+
+        const cleanup = () => {
+            box.removeEventListener('mousedown', onMouseDown);
+            box.removeEventListener('mouseup', onMouseUp);
+            container.removeEventListener('mousemove', onMouseMove);
+            container.removeEventListener('mouseleave', onMouseUp);
+        }
+
+        return cleanup;
+    }, [])
+
+    return (
+        <div ref={boxRef} className="absolute top-4 left-4 border px-4 py-2 w-fit rounded-xl cursor-pointer">
+            {text}
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,138 @@
-import Image from 'next/image'
+"use client";
+import {useRef, useState} from "react";
+import NewText from "@/app/new-text";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [texts, setTexts] = useState<string[]>([]);
+    const [currentText, setCurrentText] = useState<string>("");
+    const [removedTexts, setRemovedTexts] = useState<string[]>([]);
+    const handleAddText = (text: string) => {
+        setTexts([...texts, text]);
+    }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    return (
+        <main className={`grid grid-cols-1 md:grid-cols-3 place-items-center h-screen w-screen`}>
+            <div className={`flex flex-col h-full w-full mt-16`}>
+                <div className={`p-4 w-fit flex`}>
+                    <button className={`btn btn-outline mr-4 w-full`} onClick={
+                        () =>{
+                            // remove last text
+                            const newTexts = [...texts];
+                            const removedText = newTexts.pop();
+                            if (removedText) setRemovedTexts([...removedTexts, removedText]);
+                            setTexts(newTexts);
+                        }
+                    }>
+                        Undo
+                    </button>
+                    <button className={`btn btn-outline w-full`} onClick={
+                        () =>{
+                            // remove last text
+                            const newTexts = [...texts];
+                            const removedText = removedTexts.pop();
+                            if (removedText) setTexts([...newTexts, removedText]);
+                            setRemovedTexts(newTexts);
+                        }
+                    }>
+                        Redo
+                    </button>
+                </div>
+            </div>
+            <div ref={containerRef}
+                 className="relative border border-gray-400 rounded-xl h-[800px] w-full overflow-hidden">
+                {
+                    texts.map((text, i) => (
+                        <NewText key={i} containerRef={containerRef} text={text}/>
+                    ))
+                }
+            </div>
+            <div className={`flex flex-col h-full items-start space-y-4 w-full p-6 mt-8`}>
+                <div className={`w-full`}>
+                <h3
+                className={`font-bold text-lg mb-2`}
+                >
+                    Font
+                </h3>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                <select className={`select select-bordered w-full`}>
+                    <option value="Inter">Inter</option>
+                    <option value="Roboto">Roboto</option>
+                    <option value="Poppins">Poppins</option>
+                </select>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+                <div className={`flex w-full items-center justify-start pb-8`}>
+                    <div className={`w-full mr-4`}>
+                        <h3
+                            className={`font-bold text-lg mb-2`}
+                        >
+                            Size
+                        </h3>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                        <select className={`select select-bordered w-full`}>
+                            <option value="Inter">12</option>
+                            <option value="Roboto">14</option>
+                            <option value="Poppins">16</option>
+                        </select>
+                    </div>
+                    <div className={`w-full`}>
+                        <h3
+                            className={`font-bold text-lg mb-2`}
+                        >
+                            Color
+                        </h3>
+
+                        <select className={`select select-bordered w-full`}>
+                            <option value="Inter">Black</option>
+                            <option value="Roboto">White</option>
+                            <option value="Poppins">Red</option>
+                        </select>
+                    </div>
+                </div>
+
+                <hr className={`w-full pb-8 border-base-content/50`}/>
+
+                <button className={`btn btn-accent btn-outline w-full`} onClick={
+                    () => {
+                        (document as any).getElementById("add_new_text")?.showModal();
+                    }
+                }>
+                    Add Text
+                </button>
+            </div>
+
+            <dialog id="add_new_text" className="modal">
+            <div className="modal-box">
+                    <h3 className="font-bold text-lg">Add New Text</h3>
+                    <p className="py-4">Enter the text you want to add to the canvas.</p>
+                    <form onSubmit={
+                        (e) => {
+                            e.preventDefault();
+                            if (currentText === "") return;
+                            handleAddText(currentText);
+                            setCurrentText("");
+                            (document as any).getElementById("add_new_text")?.close();
+                        }
+                    }>
+                        <div className={`flex`}>
+                            <input
+                                value={currentText}
+                                onChange={
+                                    (e) => {
+                                        setCurrentText(e.target.value);
+                                    }
+                                }
+                                type="text" className="input input-bordered w-full mr-4" placeholder="Enter Text"/>
+                            <button className="btn">Add</button>
+                        </div>
+                    </form>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button className={`cursor-default`}>close</button>
+                </form>
+            </dialog>
+        </main>
+    );
 }
